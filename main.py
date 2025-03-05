dac5537b52a4ffa0f39bfeb00e1829506fa21786
caydnbaldwin <caydnbaldwin@gmail.com>
2025-03-04 23:44:01 -0700

    Added the rest of the functions

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 def welcome_message(name):
     print(f"Hello {name}, welcome to IS 303!")
 
+# call function
 welcome_message("Diego")
 welcome_message("Mai")
 
@@ -13,6 +14,7 @@ welcome_message("Mai")
 def sum_two_numbers(a, b):
     return a + b
 
+# call function
 print(sum_two_numbers(5, 7))
 print(sum_two_numbers(1000.5, -30))
 
@@ -20,6 +22,7 @@ print(sum_two_numbers(1000.5, -30))
 def is_even(num):
     return num % 2 == 0
 
+# call function
 print(is_even(7))
 print(is_even(120))
 
@@ -29,7 +32,8 @@ def get_number_parity(num):
         return f"{num} is even."
     else:
         return f"{num} is odd."
-    
+
+# call function
 print(get_number_parity(5))
 print(get_number_parity(10))
 
@@ -37,6 +41,7 @@ print(get_number_parity(10))
 def fahrenheit_to_celsius(fahrenheit):
     return (fahrenheit - 32) * (5 / 9)
 
+# call function
 print(fahrenheit_to_celsius(32))
 print(fahrenheit_to_celsius(75))
 
@@ -54,4 +59,41 @@ def min_max_mean(numbers_list):
         total += number    
     return [lowest_number, highest_number, (total / len(numbers_list))]
 
+# call function
 print(min_max_mean([20, 45, 23, 2, 87, 3]))
+
+# Function 7: dog_message
+def dog_message(name, age=0):
+    return f"I am a dog named {name} and I'm {age} years old!"
+
+# call function
+print(dog_message("Spot", 7))
+print(dog_message("Peppy"))
+
+# Function 8: classify_age
+def classify_age(age, senior_age=65):
+    if age < 18:
+        return "Minor"
+    elif age < senior_age:
+        return "Adult"
+    else:
+        return "Senior"
+
+# call function
+print(classify_age(60, 55))
+print(classify_age(62))
+
+# Function 9: calculate_total
+def calculate_total(price, quantity, discount_percent, threshold_total=100, bonus_discount=0.02):
+    total_price = price * quantity
+    if total_price <= threshold_total:
+        return total_price * (1 - discount_percent)
+    elif total_price > threshold_total:
+        return total_price * (1 - (discount_percent + bonus_discount))
+
+# call function
+for i in range(0, 2):
+    price = float(input("Enter the price for the product purchased: "))
+    quantity = int(input("Enter the quantity of the product purchased: "))
+    discount_percent = float(input("Enter the discount percent (formatted as a decimal): "))
+    print(f"The total price after discounts is: ${round(calculate_total(price, quantity, discount_percent), 2)}")
